a62ecedd385f0bbf265e19648f7f263c80332ae8
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2023-05-05 16:25:48 +0300

    sign up

--- auth_service/views.py
+++ auth_service/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Customer
+from .serializer  import CreateCustomerSerializer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.db.transaction import atomic
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
 
-# Create your views here.
+    return {
+        'refresh_token': str(refresh),
+        'access_token': str(refresh.access_token),
+    }
+class CreateUser(APIView):
+    def post(self,request):
+        serializer = CreateCustomerSerializer(data = request.data)
+        if serializer.is_valid():
+            with atomic():
+                User = get_user_model()
+                try:
+                    user = User.objects.create(**serializer.data)
+                except:
+
+                    return Response('user already created', status=status.HTTP_400_BAD_REQUEST)
+                user.set_password(serializer.data['password'])
+                user.save()
+                jwt_token = get_tokens_for_user(user)
+                response = Response(jwt_token, status=status.HTTP_201_CREATED)
+                response.set_cookie('access_token',jwt_token['access_token'])
+                response.set_cookie('refresh_token',jwt_token['refresh_token'])
+                return  response
+
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
