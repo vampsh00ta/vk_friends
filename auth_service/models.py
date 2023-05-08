b6c4e264ffbfa086a07d8421fd0f7cd9b88c40b5
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2023-05-08 18:50:28 +0300

    added field followed_by to Customer

--- auth_service/models.py
+++ auth_service/models.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.db.models.fields import related
+from django.db.models import Q
 # Create your models here.
+
+
+
 class Customer(AbstractUser):
-    friends = models.ManyToManyField("Customer",blank=True)
+    friends = models.ManyToManyField("Customer",blank=True,related_name='customer_friends')
+    subscribed_on = models.ManyToManyField("Customer",blank=True,related_name= "customer_subscribed_on",through = 'FriendshipRequest')
+    @property
+    def followed_by(self):
+        followed_by = Customer.objects.filter(Q(subscribed_on =self.id ) & ~Q(friends =self.id ))
+        return followed_by
     def __str__(self):
         return f"{self.id}"
+
+class FriendshipRequest(models.Model):
+    from_person = models.ForeignKey(Customer, related_name='from_person',on_delete=models.CASCADE)
+    to_person = models.ForeignKey(Customer, related_name='to_person',on_delete=models.CASCADE)
+
+
